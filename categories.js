271bcae661f555e9a6593a080ed247a366043df8
simbaa345 <82680186+simbaa345@users.noreply.github.com>
2024-10-24 11:13:34 -0400

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,12 @@
+// categories.js
+export const categories = [
+    {
+        title: "Cereal Brands",
+        items: ["Froot Loops", "Cinnamon Toast Crunch", "Honey Nut Cheerios", "Raisin Bran", "Wheaties"],
+    },
+    {
+        title: "Honeymoon Destinations",
+        items: ["Greece", "Morocco", "Canada", "Australia", "Argentina"],
+    },
+    // Add more categories as needed
+];
